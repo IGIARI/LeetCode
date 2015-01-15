66ceea43b0502e7004e32aa8e488a7546bff331b
IGIARI <song.st.87@gmail.com>
2015-01-15 14:47:35 +0900

    File reorganized

---
+++ 0011.Container-With-Most-Water/Solution.h
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+        int maxValue = 0;
+        int i = 0, j = height.size() - 1;
+        while (i < j) {
+            if (min(height[i], height[j]) * (j - i) > maxValue) {
+                    maxValue = min(height[i], height[j]) * (j - i);
+            }
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return maxValue;
+    }
+};
